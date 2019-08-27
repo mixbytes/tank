2b791361fc246cda467c6d559770b84760efc23c
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2019-08-27 16:24:38 +0300

    kill only tank children

--- tank/main.py
+++ tank/main.py
@@ -168,7 +168,7 @@ def main():
             # Default Cement signals are SIGINT and SIGTERM, exit 0 (non-error)
 
             if e.signum in {signal.SIGTERM, signal.SIGINT}:
-                tank_children = psutil.Process().children(recursive=True)
+                tank_children = psutil.Process().children()
                 for child_process in tank_children:
                     child_process.send_signal(e.signum)
 
